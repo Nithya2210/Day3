e38b2bd2137d01118bec46ae78103c53cc3f060e
Nithya2210 <98588025+Nithya2210@users.noreply.github.com>
2022-02-02 21:00:31 +0530

    Add files via upload

---
+++ Day3A.js
@@ -0,0 +1,13 @@
+//RESUME in json
+
+let resume={
+    "name":"SANKAR",
+    "qualification": "MBA",
+    "age": "32",
+    "college": "MIT",
+    "interest": "MARKETING",
+    "skills": "HR",
+    "languages": "English",
+    "native": "SALEM",
+    }
+    console.log(resume);
